7d7cf43f8e0822b16f8ebdfb0894995d649de6c3
vural <vuralsuleyman193@gmail.com>
2025-02-26 16:07:17 +0300

    proje oluşturuldu ve şifreleme servisi yapıldı

---
+++ SezarHack/src/ŞifrelemeServisi.java
@@ -0,0 +1,46 @@
+public class ŞifrelemeServisi
+{
+    String SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public String encrypt(String message, int key) {
+        int mesajUzunluk = message.length();
+        String sonuc = "";
+
+        for (int i = 0; i < mesajUzunluk; i++) {
+            char harf = message.charAt(i);
+            if (harf == ' ') {
+                sonuc += " ";
+                continue;
+            }
+
+            char buyukHarf = Character.toUpperCase(harf);
+            int index = SYMBOLS.indexOf(buyukHarf);
+            int yeniIndex = (index + key) % SYMBOLS.length();
+            char yeniHarf = SYMBOLS.charAt(yeniIndex);
+            sonuc += yeniHarf;
+        }
+
+        return sonuc;
+    }
+    public String decript(String message, int key) {
+        int mesajUzunluk = message.length();
+        String sonuc = "";
+
+        for (int i = 0; i < mesajUzunluk; i++) {
+            char harf = message.charAt(i);
+            if (harf == ' ') {
+                sonuc += " ";
+                continue;
+            }
+
+            char buyukHarf = Character.toUpperCase(harf);
+            int index = SYMBOLS.indexOf(buyukHarf);
+            int yeniIndex = ((index + key)+SYMBOLS.length()) % SYMBOLS.length();
+            char yeniHarf = SYMBOLS.charAt(yeniIndex);
+            sonuc += yeniHarf;
+        }
+
+        return sonuc;
+    }
+}
+
