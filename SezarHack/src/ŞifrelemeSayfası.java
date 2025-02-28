a9acf7d57517ab3bbe7347f9dbdeb7d54238458d
vural <vuralsuleyman193@gmail.com>
2025-02-28 18:02:34 +0300

    proje maven olarak tekrar başladı

--- SezarHack/src/ŞifrelemeSayfası.java
+++ SezarHack/src/ŞifrelemeSayfası.java
@@ -12,23 +12,37 @@ public class ŞifrelemeSayfası implements ActionListener
     JLabel mes = new JLabel("MESAJ : ");
     JLabel anahtar = new JLabel("Anahtar : ");
     JButton gönder = new JButton("Şifrele");
+    JPanel sol =new JPanel();
+    JPanel üst = new JPanel();
+    JPanel alt = new JPanel();
+    JPanel sağ = new JPanel();
+    String SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public ŞifrelemeSayfası()
     {
-        frame.setSize(500,500);
-        frame.setLayout(new FlowLayout());
+        frame.setSize(1200,800);
+        frame.setLayout(new GridLayout(1,2));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        sol.setPreferredSize(new Dimension(600,800));
+        sol.setLayout(new FlowLayout());
+
+        üst.setPreferredSize(new Dimension(600,600));
+        üst.setLayout(new FlowLayout());
+        üst.add(mes);
+        üst.add(mesaj);
+        üst.add(anahtar);
+        üst.add(key);
+        üst.add(gönder);
 
         mesaj.setPreferredSize(new Dimension(40,150));
         key.setPreferredSize(new Dimension(40,150));
         mes.setFont(new Font("Arial",Font.BOLD,15));
         anahtar.setFont(new Font("Arial",Font.BOLD,15));
         gönder.addActionListener(this);
-        frame.add(mes);
-        frame.add(mesaj);
-        frame.add(anahtar);
-        frame.add(key);
-        frame.add(gönder);
+
+        frame.add(üst);
+
 
         frame.setVisible(true);
     }
@@ -42,9 +56,24 @@ public class ŞifrelemeSayfası implements ActionListener
             String mesajtex = mesaj.getText();
             String  keytex = key.getText();
 
-            ŞifrelemeServisi servis = new ŞifrelemeServisi();
+            int mesajUzunluk = mesajtex.length();
+            String sonuc = "";
 
-            servis.encrypt(mesajtex,Integer.parseInt(keytex));
+            for (int i = 0; i < mesajUzunluk; i++) {
+                char harf = mesajtex.charAt(i);
+                if (harf == ' ') {
+                    sonuc += " ";
+                    continue;
+                }
+
+                char buyukHarf = Character.toUpperCase(harf);
+                int index = SYMBOLS.indexOf(buyukHarf);
+                int yeniIndex = (index + Integer.parseInt(keytex)) % SYMBOLS.length();
+                char yeniHarf = SYMBOLS.charAt(yeniIndex);
+                sonuc += yeniHarf;
+            }
+
+
         }
 
     }
